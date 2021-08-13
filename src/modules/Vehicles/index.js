fc36f38d3b932a63d2dd56373482d0677470da73
Yimura <andreas.maerten@scarlet.be>
2021-08-13 18:34:31 +0200

    feat(Modules): Added Vehicle module
    Includes ShareCode util

---
+++ src/modules/Vehicles/index.js
@@ -0,0 +1,41 @@
+import BaseModule from './structures/BaseModule.js'
+import Modules from '@/src/managers/Modules.js'
+import HandlingModel from './structures/models/HandlingModel.js'
+import { createShareCode } from './util/ShareCode.js'
+
+export default class Vehicles extends BaseModule {
+    constructor(main) {
+        super(main);
+
+        this.register(Vehicles, {
+            name: 'Vehicles',
+
+            requires: [ 'mongodb', 'Sessions' ]
+        });
+    }
+
+    getHandlingDataBySession(sessionId, handling_hash) {
+        const session = Modules.Sessions.get(sessionId);
+        if (!session) return [];
+
+        return HandlingModel.getAll({ user_id: session.userId, handling_hash });
+    }
+
+    getHandlingDataByShareCode(share_code) {
+        return HandlingModel.getOne({ share_code });
+    }
+
+    async saveHandlingData(sessionId, data) {
+        const session = Modules.Sessions.get(sessionId);
+        if (!session) return undefined;
+
+        let share_code;
+        do {
+            share_code = createShareCode(session);
+        } while (!await HandlingModel.ensureUnique(share_code));
+
+        const handlingData = Object.assign({ user_id: session.userId, share_code }, data);
+
+        return HandlingModel.create(handlingData);
+    }
+}
