3a1b50997d6f41ada1b6f206298cd6911b5ba672
Yimura <andreas.maerten@scarlet.be>
2021-08-13 18:34:05 +0200

    feat(Handling): Added model

---
+++ src/modules/Vehicles/structures/models/HandlingModel.js
@@ -0,0 +1,24 @@
+import HandlingSchema from '../schemas/HandlingSchema.js'
+
+export const create = (handling) => {
+    return new HandlingSchema(handling).save();
+};
+
+export const ensureUnique = async (share_code) => {
+    return 0 == await HandlingSchema.countDocuments({ share_code });
+};
+
+export const getAll = async (q) => {
+    return HandlingSchema.find(q);
+};
+
+export const getOne = async (q) => {
+    return HandlingSchema.findOne(q);
+};
+
+export default {
+    create,
+    ensureUnique,
+    getAll,
+    getOne
+}
